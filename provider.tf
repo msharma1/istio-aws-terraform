5305ba9a274d782eec2964d90a143f610e8709ac
msharma1 <manish.sharma0201cs@gmail.com>
2023-02-06 00:58:19 +0000

    Fixing syntax

--- provider.tf
+++ provider.tf
@@ -1,4 +1,5 @@
 
+/*
 provider "kubernetes" {
   host                   = data.aws_eks_cluster.default.endpoint
   cluster_ca_certificate = base64decode(data.aws_eks_cluster.default.certificate_authority[0].data)
@@ -12,3 +13,4 @@ provider "helm" {
     token                  = data.aws_eks_cluster_auth.default.token
   }
 }
+*/
