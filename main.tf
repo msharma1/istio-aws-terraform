06939a4b26d02244855b708701f9bc52b44fc46e
msharma1 <manish.sharma0201cs@gmail.com>
2023-02-06 01:52:52 +0000

    adding default name for aws provider

--- main.tf
+++ main.tf
@@ -3,15 +3,11 @@ locals {
 }
 
 terraform {
-  backend "s3" {
-    bucket = "istio-tf-backend"
-    key    = "terraform.tfstate"
-    region = "eu-west-1"
-  }
 }
 
 provider "aws" {
   region = local.region
+  alias  = "default"
 }
 
 module "vpc" {
