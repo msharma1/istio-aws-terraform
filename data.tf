4ecb9c03f8b608d91dbfe2095658160bc0843468
msharma1 <manish.sharma0201cs@gmail.com>
2023-02-06 01:56:01 +0000

    Removing depends_on

--- data.tf
+++ data.tf
@@ -1,9 +1,7 @@
 data "aws_eks_cluster" "default" {
   name = var.cluster_name
-  depends_on = [module.eks]
 }
 
 data "aws_eks_cluster_auth" "default" {
   name = var.cluster_name
-  depends_on = [module.eks]
 }
